e355a3cb16a7f6968438121ad0a31fdf8a00c0bd
Guilherme Passarinho <guilhermepassarinho@gmail.com>
2020-01-15 03:50:28 -0300

    finalizado backend

---
+++ backend/src/index.js
@@ -0,0 +1,29 @@
+//Metodos HTTP: GET, POST, PUT, DELETE
+
+//Tipos de parametros: 
+
+//Query Params: request.query (Filtros, ordenação, paginação)
+//Route Params: request.params (Identificar um recurso na alteração ou remoção)
+//Body: request.body (Dados para criação ou alteração de um registro)
+
+//MongoDB (BD não-relacional)
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('../src/routes');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://omni:omni@aircnc-hncqq.mongodb.net/week10?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+});
+
+app.use(express.json());
+app.use(routes);
+
+
+
+
+app.listen(3333);
